c1209287037d66ec8d500f0f50943e07b443ec0a
Brett Clifton <bclifton@adadapted.com>
2022-04-20 11:41:21 -0400

    feat: first pass deeplink impl

---
+++ aamsdk/src/androidMain/kotlin/com/adadapted/library/view/DeeplinkInterceptActivity.kt
@@ -0,0 +1,36 @@
+package com.adadapted.library.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.adadapted.library.atl.AddItContentPublisher
+import com.adadapted.library.atl.DeeplinkContentParser
+import com.adadapted.library.constants.Config.LOG_TAG
+import com.adadapted.library.constants.EventStrings
+import com.adadapted.library.payload.PayloadClient
+
+class DeeplinkInterceptActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println(LOG_TAG + "Deeplink Intercept Activity Launched.")
+        PayloadClient.getInstance().deeplinkInProgress()
+        //AppEventClient.getInstance().trackSdkEvent(EventStrings.ADDIT_APP_OPENED)
+
+        try {
+            val content = DeeplinkContentParser().parse(intent.data)
+            println(LOG_TAG + "AddIt content dispatched to App.")
+            AddItContentPublisher.getInstance().publishAddItContent(content)
+        } catch (ex: Exception) {
+            println(LOG_TAG + "Problem dealing with AddIt content. Recovering. " + ex.message)
+            val errorParams: MutableMap<String, String> = HashMap()
+            ex.message?.let { errorParams.put(EventStrings.EXCEPTION_MESSAGE, it) }
+//            AppEventClient.getInstance().trackError(
+//                EventStrings.ADDIT_DEEPLINK_HANDLING_ERROR,
+//                "Problem handling deeplink",
+//                errorParams
+//            )
+            startActivity(packageManager.getLaunchIntentForPackage(packageName))
+        }
+        PayloadClient.getInstance().deeplinkCompleted()
+        finish()
+    }
+}
