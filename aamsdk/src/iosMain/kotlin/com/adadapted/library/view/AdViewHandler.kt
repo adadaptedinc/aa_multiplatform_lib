1f653a9d780d33ff9277d8b4b0df5dbece2f60af
Matthew Kruk <mkruk@adadapted.com>
2022-07-26 15:43:33 -0400

    feat: refactor and add swiftpackage zip

--- aamsdk/src/iosMain/kotlin/com/adadapted/library/view/AdViewHandler.kt
+++ aamsdk/src/iosMain/kotlin/com/adadapted/library/view/AdViewHandler.kt
@@ -4,10 +4,10 @@ import com.adadapted.library.ad.Ad
 
 actual class AdViewHandler {
     actual fun handleLink(ad: Ad) {
-        LinkOrPopup(ad = ad, linkType = LinkType.EXTERNAL)
+        Popup(ad = ad, linkType = LinkType.EXTERNAL)
     }
 
     actual fun handlePopup(ad: Ad) {
-        LinkOrPopup(ad = ad, linkType = LinkType.POP_UP)
+        Popup(ad = ad, linkType = LinkType.POP_UP)
     }
 }
