4e0a7335fae37145aac1772f52c4b743882c079c
Brett Clifton <bclifton@adadapted.com>
2022-09-26 11:11:25 -0400

    feat: Ad unit tests

--- aamsdk/src/commonTest/kotlin/com/adadapted/library/commonAdTest.kt
+++ aamsdk/src/commonTest/kotlin/com/adadapted/library/commonAdTest.kt
@@ -1,24 +1,52 @@
 package com.adadapted.library
 
 import com.adadapted.library.ad.Ad
-import kotlin.test.Test
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import com.adadapted.library.atl.AddToListItem
+import com.adadapted.library.payload.Payload
+import kotlin.test.*
 
 class CommonAdTest {
 
+    private var testAd = Ad(
+        id = "testAdId", impressionId = "123456:00000", "urlString", "atl", "a", Payload(
+            "payloadId", "pMsg", "pImg", "campaignId", "appId", 60, listOf(
+                AddToListItem(
+                    "trackingId",
+                    "testTitle",
+                    "testBrand",
+                    "testCat",
+                    "productUpc",
+                    "retailSku",
+                    "retailId",
+                    "prodImg"
+                )
+            )
+        ), 30
+    )
+
     @Test
-    fun testAdExample() {
-        val testAd = Ad()
-        assertTrue(testAd.isEmpty)
+    fun adZoneIsSplitCorrectly() {
+        assertEquals("123456", testAd.zoneId)
     }
 
     @Test
-    fun testAdExample2() {
-        val testAd = Ad()
+    fun emptyAdIdReturnsEmpty() {
+        val emptyAd = Ad()
+        assertTrue { emptyAd.isEmpty }
+    }
+
+    @Test
+    fun adImpressionSetsTracking() {
         testAd.setImpressionTracked()
         assertTrue(testAd.impressionWasTracked())
         testAd.resetImpressionTracking()
         assertFalse(testAd.impressionWasTracked())
+    }
+
+    @Test
+    fun adContentIsCreatedProperly() {
+        val testAdContent = testAd.getContent()
+        assertEquals("testTitle", testAdContent.getItems().first().title)
+        assertEquals("123456", testAdContent.zoneId)
     }
 }
